0b3991672d1d0f9ae16683192c0da0c96007141b
Shuhang09 <18126252375@163.com>
2024-01-03 18:53:28 -0500

    Fixing the miss-spelling issue and some checkstyle

--- Dockerfile
+++ Dockerfile
@@ -24,8 +24,8 @@ RUN python -m venv /py && \
     adduser \
         --disabled-password \
         --no-create-home \
-        djingo-user
+        django-user
 
 ENV PATH="/py/bin:$PATH"
 
-USER djingo-user
+USER django-user
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,20 +8,21 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """ Django command to wait for database."""
 
     def handle(self, *args, **options):
+        pass
+        """Entrypoint for command."""
         self.stdout.write("Waiting for database...")
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
-                dp_up = True
+                db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Databse available!'))
-
-
